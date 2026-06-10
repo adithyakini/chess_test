050b9fcf2a42da9383d39b92acc142ab16a9994a
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-10 16:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from stockfish import Stockfish
 
-st.title("Stockfish Debug")
+st.title("♟️ Stockfish Cloud Test")
 
 try:
 
@@ -9,7 +9,19 @@ try:
         path="/usr/games/stockfish"
     )
 
-    st.write(dir(stockfish))
+    stockfish.set_fen_position(
+        "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1"
+    )
+
+    best_move = stockfish.get_best_move()
+
+    evaluation = stockfish.get_evaluation()
+
+    st.success(f"Best move: {best_move}")
+
+    st.write("Evaluation:")
+
+    st.json(evaluation)
 
 except Exception as e:
 
