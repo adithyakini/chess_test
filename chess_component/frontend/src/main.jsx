4a908ed95768ddcbe4336dd6f734193187bc5e30
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-11 02:56:47 +0530

    Update main.jsx

--- chess_component/frontend/src/main.jsx
+++ chess_component/frontend/src/main.jsx
@@ -1,20 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
-import {
-  Streamlit,
-  withStreamlitConnection
-} from "streamlit-component-lib";
-
-import ChessBoard from "./ChessBoard";
-
-const ConnectedBoard =
-  withStreamlitConnection(
-    ChessBoard
-  );
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <ConnectedBoard />
+  <h1>HELLO FROM COMPONENT</h1>
 );
