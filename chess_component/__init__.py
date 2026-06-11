306b717816382fd24e4c23f996cd3b4bcb7bea28
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-11 09:24:14 +0530

    Update __init__.py

--- chess_component/__init__.py
+++ chess_component/__init__.py
@@ -1,9 +1,8 @@
 import os
+import streamlit as st
 import streamlit.components.v1 as components
 
-parent_dir = os.path.dirname(
-    os.path.abspath(__file__)
-)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
 
 build_dir = os.path.join(
     parent_dir,
@@ -11,15 +10,16 @@ build_dir = os.path.join(
     "dist"
 )
 
-_component_func = (
-    components.declare_component(
-        "chessboard",
-        path=build_dir
-    )
+print("BUILD DIR:", build_dir)
+print("EXISTS:", os.path.exists(build_dir))
+print("INDEX:", os.path.exists(os.path.join(build_dir, "index.html")))
+
+_component_func = components.declare_component(
+    "chessboard",
+    path=build_dir
 )
 
 def st_chessboard(fen="start"):
-
     return _component_func(
         fen=fen,
         default=None
