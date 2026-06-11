d02d67bec3bfefa5c750aca49adbc4473f96e351
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-11 09:19:03 +0530

    Update main.jsx

--- chess_component/frontend/src/main.jsx
+++ chess_component/frontend/src/main.jsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <h1>HELLO FROM COMPONENT</h1>
+  <div>
+    <h1>HELLO FROM COMPONENT</h1>
+    <p>If you can read this, React is working.</p>
+  </div>
 );
